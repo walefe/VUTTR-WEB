4960b8ee4cdaea2f835c276dae29717de3024397
Walefe <walefedev@gmail.com>
2019-11-09 14:22:27 -0300

    add unform.

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/tools.svg';
 
 export default function SingIn() {
+  function handelSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Tools" />
 
-      <form>
-        <input type="email" placeholder="e-mail..." />
+      <Form onSubmit={handelSubmit}>
+        <Input name="email" type="email" placeholder="e-mail..." />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita.</Link>
-      </form>
+      </Form>
     </>
   );
 }
