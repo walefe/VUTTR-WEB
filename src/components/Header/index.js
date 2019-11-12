ee14728e15afbbde9fbfefb832ca17a1b804d6bf
Walefe <walefedev@gmail.com>
2019-11-12 20:18:22 -0300

    Chore: add head to application.

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/tools.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="VUTTR" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Walefe</strong>
+            </div>
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
