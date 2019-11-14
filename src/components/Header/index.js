8610c34b7c8b7c967f432d9ac42735ece1315086
Walefe <walefedev@gmail.com>
2019-11-14 12:50:26 -0300

    add button exit to application.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,6 +19,7 @@ export default function Header() {
             <div>
               <strong>Walefe</strong>
             </div>
+            <button type="button">Sair</button>
           </Profile>
         </aside>
       </Content>
