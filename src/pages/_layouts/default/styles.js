9e075f98e1fce01b6083d2e35fa138c9c5240694
Walefe <walefedev@gmail.com>
2019-11-18 18:47:06 -0300

    fix min height in wrapper application.

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  height: auto;
+  min-height: 100%;
   background: linear-gradient(180deg, #353372, #6554c0);
 `;
