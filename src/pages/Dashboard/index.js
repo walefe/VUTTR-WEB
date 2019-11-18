6e134e97f22b232e5e58dc0052beef61414e3ead
Walefe <walefedev@gmail.com>
2019-11-18 18:50:08 -0300

    Chore: make a tools api call.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IoIosAdd, IoIosClose } from 'react-icons/io';
+
+import api from '~/services/api';
 
 import { Container, Header, Content, Tool } from './styles';
 
 export default function Dashboard() {
+  const [tools, setTools] = useState([]);
+
+  useEffect(() => {
+    async function loadTools() {
+      const response = await api.get('tools');
+
+      setTools(response.data);
+    }
+    loadTools();
+  }, []);
+
   return (
     <Container>
       <Content>
@@ -25,121 +38,22 @@ export default function Dashboard() {
           </nav>
         </Header>
 
-        <Tool>
-          <li>
-            <div>
-              <a href="teste">TESTE</a>
-              <button type="button">
-                <IoIosClose size={20} />
-              </button>
-            </div>
-            <p>
-              All in one tool to organize teams and ideas. Write, plan,
-              collaborate, and get organized.
-            </p>
-          </li>
-        </Tool>
-
-        <Tool>
-          <li>
-            <div>
-              <a href="teste">TESTE</a>
-              <button type="button">
-                <IoIosClose size={20} />
-              </button>
-            </div>
-            <p>
-              All in one tool to organize teams and ideas. Write, plan,
-              collaborate, and get organized. All in one tool to organize teams
-              and ideas. Write, plan, collaborate, and get organized.
-            </p>
-          </li>
-        </Tool>
-
-        <Tool>
-          <li>
-            <div>
-              <a href="teste">TESTE</a>
-              <button type="button">
-                <IoIosClose size={20} />
-              </button>
-            </div>
-            <p>
-              All in one tool to organize teams and ideas. Write, plan,
-              collaborate, and get organized.
-            </p>
-          </li>
-        </Tool>
-        <Tool>
-          <li>
-            <div>
-              <a href="teste">TESTE</a>
-              <button type="button">
-                <IoIosClose size={20} />
-              </button>
-            </div>
-            <p>
-              All in one tool to organize teams and ideas. Write, plan,
-              collaborate, and get organized.
-            </p>
-          </li>
-        </Tool>
-        <Tool>
-          <li>
-            <div>
-              <a href="teste">TESTE</a>
-              <button type="button">
-                <IoIosClose size={20} />
-              </button>
-            </div>
-            <p>
-              All in one tool to organize teams and ideas. Write, plan,
-              collaborate, and get organized.
-            </p>
-          </li>
-        </Tool>
-        <Tool>
-          <li>
-            <div>
-              <a href="teste">TESTE</a>
-              <button type="button">
-                <IoIosClose size={20} />
-              </button>
-            </div>
-            <p>
-              All in one tool to organize teams and ideas. Write, plan,
-              collaborate, and get organized.
-            </p>
-          </li>
-        </Tool>
-        <Tool>
-          <li>
-            <div>
-              <a href="teste">TESTE</a>
-              <button type="button">
-                <IoIosClose size={20} />
-              </button>
-            </div>
-            <p>
-              All in one tool to organize teams and ideas. Write, plan,
-              collaborate, and get organized.
-            </p>
-          </li>
-        </Tool>
-        <Tool>
-          <li>
-            <div>
-              <a href="teste">TESTE</a>
-              <button type="button">
-                <IoIosClose size={20} />
-              </button>
-            </div>
-            <p>
-              All in one tool to organize teams and ideas. Write, plan,
-              collaborate, and get organized.
-            </p>
-          </li>
-        </Tool>
+        <ul>
+          {tools.map(tool => (
+            <Tool key={tool.id}>
+              <div>
+                <a href={tool.link}>{tool.title}</a>
+                <button type="button">
+                  <IoIosClose size={20} />
+                </button>
+              </div>
+              <p>{tool.description}</p>
+              {tool.tags.map(tag => (
+                <span>{tag}</span>
+              ))}
+            </Tool>
+          ))}
+        </ul>
       </Content>
     </Container>
   );
