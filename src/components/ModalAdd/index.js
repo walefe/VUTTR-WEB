df2c1d1502f33e926feb950b28664dd73f3227de
Walefe <walefedev@gmail.com>
2019-11-21 17:13:45 -0300

    Chore: add a new tool.

--- src/components/ModalAdd/index.js
+++ src/components/ModalAdd/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 import { Input } from '@rocketseat/unform';
 import { IoIosClose } from 'react-icons/io';
 import * as Yup from 'yup';
+
+import api from '~/services/api';
 
 import { ModalsAdd, Content, FormAdd, InputAdd } from './styles';
 
@@ -16,6 +19,26 @@ const schema = Yup.object().shape({
 });
 
 export default function ModalAdd({ modalAddOpen, closeModalAdd }) {
+  async function handleSubmit({ title, link, description, tags }) {
+    try {
+      const tool = {
+        title,
+        link,
+        description,
+        tags: tags.split(' '),
+      };
+
+      await api.post('tools', tool);
+
+      document.location.reload(true);
+
+      closeModalAdd();
+    } catch (err) {
+      toast.error('Unable to add a new tool');
+      closeModalAdd();
+    }
+  }
+
   return (
     <ModalsAdd
       isOpen={modalAddOpen}
@@ -29,7 +52,7 @@ export default function ModalAdd({ modalAddOpen, closeModalAdd }) {
             <IoIosClose size={20} />
           </button>
         </div>
-        <FormAdd schema={schema}>
+        <FormAdd schema={schema} onSubmit={handleSubmit}>
           <InputAdd name="title" type="title" placeholder="Tool" />
           <InputAdd name="link" type="link" placeholder="Tool Link" />
           <Input
